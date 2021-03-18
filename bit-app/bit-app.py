fdc4fe26c1227a26c1e1aa49441bc3d5e0b58238
Wei-Ting Yap <weitingyp@gmail.com>
2021-03-17 21:50:43 -0700

    Created new subscriber API (route subscribe) and adapted HTML to Jinja template
    - New route '/api/subscribe/' for subscribe form submission created,
      to be updated to store email address
    - Updated URL for CSS style sheet to Jinja component
    - Added form action and method, set to route for new subscriber

--- bit-app/bit-app.py
+++ bit-app/bit-app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# landing page
 @app.route('/')
 def index():
+    """
+    Route for landing page
+    """
+    print("LOADING HERE")
     return render_template('index.html')
+
+@app.route('/api/subscribe/', methods=['POST'])
+def subscribe():
+    """
+    API that retrieves submitted form data for a new subscriber
+    """
+    email = request.form['email']
+    print(email)
+    return email
